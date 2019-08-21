9efad28e28db02def5d74fbd94d098c599de45f5
petercharles20 <ps952@uowmail.edu.au>
2019-08-21 19:37:20 +1000

    Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NgXformModule } from '@esss/ng-xform';
+import { SortComponent } from './sort/sort.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    SortComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgXformModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
